e0de510572d20bd7a80c4e5f635e402a25606ffb
matias chattellino <matiaschattellino@gmail.com>
2020-06-30 17:19:00 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,63 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import Services from "../components/Services"
+import Jobs from "../components/Jobs"
+import Projects from "../components/Projects"
+import Blogs from "../components/Blogs"
+export default ({data}) => {
+  const { allStrapiProjects:{nodes:projects} } = data;
+
+  return (
+      <Layout>
+          <Hero  />
+          <Services />
+          <Jobs />  
+          <Projects projects={projects} title="featured projects" showLink />
+      </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: {featured: {eq: true}}) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+      nodes {
+        slug
+        content
+        desc
+        date(formatString: "MMMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
